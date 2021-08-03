1c24ede22787b2309cebd45fa956c9438f97c3ec
Anurag Singh <contact@anurag.sh>
2021-08-03 16:45:32 +0800

    Add sentry, improve dockerfile
    Sentry is blocked by ublock origin :)

--- frontend/clients/paste/src/index.tsx
+++ frontend/clients/paste/src/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-less/semantic.less';
+
+Sentry.init({
+  enabled: process.env.NODE_ENV !== 'development',
+  environment: process.env.NODE_ENV,
+  dsn: 'https://ecb66ab96df441bdb455c1a2b758c09f@o931028.ingest.sentry.io/5879810',
+  integrations: [new Integrations.BrowserTracing()],
+});
 
 ReactDOM.render(
   <React.StrictMode>
