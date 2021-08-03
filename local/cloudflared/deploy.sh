fc6dbd955e29427032e5c52111a0e1e3e1baaf13
Anurag Singh <contact@anurag.sh>
2021-08-03 17:26:24 +0800

    add cloudflared tunnel config

---
+++ local/cloudflared/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+REMOTE_USER=arch
+CLOUDFLARED_LOCAL="$HOME/.cloudflared"
+
+if [ -z "$1" ]; then
+    echo "$0 [ip address]"
+    exit 1
+fi
+
+# 10.130.146.225
+SERVER="$1"
+rsync \
+    -avz \
+    --progress \
+    "$CLOUDFLARED_LOCAL" \
+    "$REMOTE_USER@$SERVER:/home/$REMOTE_USER"
+
+rsync \
+    -avz \
+    --progress \
+    --rsync-path "sudo rsync" \
+    "./config.yml" \
+    "$REMOTE_USER@$SERVER:/etc/cloudflared"
+
+echo "---> systemctl restart cloudflared"
+ssh "$REMOTE_USER@$SERVER"
