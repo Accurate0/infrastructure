9056e9775ea534807de26be403891ba892ed0c19
Anurag Singh <contact@anurag.sh>
2021-11-21 20:24:26 +0800

    create second oracle vms (it's free!!!0

--- cloud/oracle/tf/instance.tf
+++ cloud/oracle/tf/instance.tf
@@ -32,3 +32,24 @@ resource "oci_core_instance" "ubuntu_paste" {
     user_data           = base64encode(data.template_file.user_data.rendered)
   }
 }
+
+resource "oci_core_instance" "ubuntu_buildkite" {
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  compartment_id      = oci_identity_compartment.paste.id
+  shape               = "VM.Standard.E2.1.Micro"
+  source_details {
+    source_id   = local.ubuntu_image.source_id
+    source_type = "image"
+  }
+
+  display_name = "buildkite_vms"
+  create_vnic_details {
+    assign_public_ip = true
+    subnet_id        = oci_core_subnet.paste-public-subnet.id
+  }
+
+  metadata = {
+    ssh_authorized_keys = file("./instance_key.pub")
+    user_data           = base64encode(data.template_file.user_data.rendered)
+  }
+}
--- cloud/oracle/tf/output.tf
+++ cloud/oracle/tf/output.tf
@@ -1,3 +1,7 @@
-output "public-ip" {
+output "public-ip-paste" {
   value = oci_core_public_ip.paste-public-ip.ip_address
+}
+
+output "buildkite-ip-paste" {
+  value = oci_core_instance.ubuntu_buildkite.public_ip
 }
