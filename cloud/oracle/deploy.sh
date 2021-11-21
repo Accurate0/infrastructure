843866a817660a27624936b3ac854f62c1963590
Anurag Singh <contact@anurag.sh>
2021-11-21 21:27:42 +0800

    move files to oracle vms

--- cloud/oracle/deploy.sh
+++ cloud/oracle/deploy.sh
@@ -13,29 +13,54 @@ RAW_IMAGE=image.tar
 PROJ=oracle
 REMOTE_USER=ubuntu
 PRIVATE_KEY="./tf/instance_key"
-PUBLIC_IP=$(cd tf && terraform output -raw public-ip)
+PUBLIC_IP_PASTE=$(cd tf && terraform output -raw public-ip-paste)
+PUBLIC_IP_BUILDKITE=$(cd tf && terraform output -raw public-ip-buildkite)
 SSH_COMMAND="ssh -q -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $PRIVATE_KEY"
 
 set -x
 
-docker-compose -p "$PROJ" build
-docker save -o "$RAW_IMAGE" "${PROJ}_frontend" "${PROJ}_paste" "${PROJ}_redis"
+# docker-compose -p "$PROJ" build
+# docker save -o "$RAW_IMAGE" "${PROJ}_frontend" "${PROJ}_paste" "${PROJ}_redis"
 
-zstd -T0 -20 --ultra --rsyncable "$RAW_IMAGE" -o "$RAW_IMAGE.zst"
+# zstd -T0 -20 --ultra --rsyncable "$RAW_IMAGE" -o "$RAW_IMAGE.zst"
 
-rsync \
-    -avz \
-    -e "$SSH_COMMAND" \
-    --progress \
-    ./$RAW_IMAGE.zst \
-    ./docker-compose.yml \
-    "$REMOTE_USER@$PUBLIC_IP:/home/$REMOTE_USER/app"
+# transfer_paste() {
+#     rsync \
+#         -avz \
+#         -e "$SSH_COMMAND" \
+#         --progress \
+#         ./$RAW_IMAGE.zst \
+#         ./docker-compose.yml \
+#         "$REMOTE_USER@$PUBLIC_IP_PASTE:/home/$REMOTE_USER/app"
 
-$SSH_COMMAND "$REMOTE_USER@$PUBLIC_IP" "bash -s" << EOF
-set -x
-cd app
-zstd -f -d -T0 "$RAW_IMAGE".zst -o "$RAW_IMAGE"
-docker load -i "$RAW_IMAGE"
-docker-compose -p "$PROJ" up -d --no-build
-docker ps
+#     $SSH_COMMAND "$REMOTE_USER@$PUBLIC_IP_PASTE" "bash -s" << EOF
+#     set -x
+#     cd app
+#     zstd -f -d -T0 "$RAW_IMAGE".zst -o "$RAW_IMAGE"
+#     docker load -i "$RAW_IMAGE"
+#     docker-compose -p "$PROJ" up -d --no-build
+#     docker ps
+# EOF
+# }
+
+
+transfer_buildkite() {
+    rsync \
+        -avz \
+        -e "$SSH_COMMAND" \
+        --progress \
+        "../../services/files" \
+        "$REMOTE_USER@$PUBLIC_IP_BUILDKITE:/home/$REMOTE_USER/app"
+
+    $SSH_COMMAND "$REMOTE_USER@$PUBLIC_IP_BUILDKITE" "bash -s" << EOF
+    set -x
+    cd app/files
+    docker-compose up --build -d
+    docker-compose ps
 EOF
+}
+
+# transfer_paste &
+transfer_buildkite &
+
+wait
--- cloud/oracle/tf/output.tf
+++ cloud/oracle/tf/output.tf
@@ -2,6 +2,6 @@ output "public-ip-paste" {
   value = oci_core_public_ip.paste-public-ip.ip_address
 }
 
-output "buildkite-ip-paste" {
+output "public-ip-buildkite" {
   value = oci_core_instance.ubuntu_buildkite.public_ip
 }
