c9adae831d887f03f441fe1a6f3ca257dc14ad6d
Anurag Singh <contact@anurag.sh>
2022-01-01 18:17:01 +0800

    use jenkins user

--- cloud/deploy_buildmachine.sh
+++ cloud/deploy_buildmachine.sh
@@ -16,6 +16,9 @@ $SSH_COMMAND "$REMOTE_USER@$PUBLIC_IP_JENKINS" "bash -s" << EOF
 mkdir -p /srv/buildmachine
 cd /srv/buildmachine
 
+useradd -m jenkins || exit 0
+su jenkins
+
 echo $JENKINS_EXEC_SECRET > jenkins_secret
 pkill -f -9 agent.jar
 nohup java -jar agent.jar -jnlpUrl https://jenkins.anurag.sh/computer/linode1/jenkins-agent.jnlp -secret @jenkins_secret -workDir "/var/jenkins" &
