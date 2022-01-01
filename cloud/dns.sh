a4fb372773308eac1cc854c419cc2e8c5b3d9173
Anurag Singh <contact@anurag.sh>
2022-01-02 00:14:01 +0800

    don't forget to activate cloudflare

--- cloud/dns.sh
+++ cloud/dns.sh
@@ -17,5 +17,5 @@ cfcli -t A -d 'anurag.sh' add oracle1 "$(terraform output -raw public-ip-paste)"
 cfcli -t A -d 'anurag.sh' add oracle2 "$(terraform output -raw public-ip-buildkite)"
 
 cfcli -t CNAME -d 'anurag.sh' add files oracle2.anurag.sh || exit 0
-cfcli -t CNAME -d 'anurag.sh' add paste oracle1.anurag.sh || exit 0
-cfcli -t CNAME -d 'anurag.sh' add jenkins linode2.anurag.sh || exit 0
+cfcli -t CNAME -d 'anurag.sh' -a add paste oracle1.anurag.sh || exit 0
+cfcli -t CNAME -d 'anurag.sh' -a add jenkins linode2.anurag.sh || exit 0
