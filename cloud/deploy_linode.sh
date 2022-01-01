42e4a6bafd883d1ef6287f3d435fe063f74c7f60
Anurag Singh <contact@anurag.sh>
2022-01-01 15:19:57 +0800

    update

--- cloud/deploy_linode.sh
+++ cloud/deploy_linode.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env bash
+# manual deployment, please dont hurt me
+set -eo pipefail
 
-exit 0
+KEY="./terraform/instance_key"
+
+[ -f "$KEY" ] || echo "$INSTANCE_KEY" >> "$KEY"
+
+ORIGIN_CERT="../nginx/certs/anurag.sh.key"
+[ -f "$ORIGIN_CERT" ] || echo "$ORIGIN_SSL_CERT" >> "$ORIGIN_CERT"
+
+chmod 400 "$KEY"
+
+export DOCKER_BUILDKIT=1
+REMOTE_USER=root
+PRIVATE_KEY="$KEY"
+PUBLIC_IP_JENKINS="linode2.anurag.sh"
+SSH_COMMAND="ssh -q -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i $PRIVATE_KEY"
+
+set -x
+
+REMOTE_USER=root
+rsync \
+    -e "$SSH_COMMAND" \
+    -avzr \
+    --progress \
+    "../services/jenkins" \
+    "$REMOTE_USER@$PUBLIC_IP_JENKINS:/srv"
+
+$SSH_COMMAND "$REMOTE_USER@$PUBLIC_IP_JENKINS" "bash -s" << EOF
+set -x
+cd /srv/jenkins
+docker-compose up -d
+EOF
+# jenkins-jobs --conf ./config.ini update --workers 0 --delete-old ./
