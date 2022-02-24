967484f8b1ba23990dda7faba059095666b996f5
Anurag Singh <contact@anurag.sh>
2022-02-24 22:32:26 +0800

    try with script

--- cloud/deploy_paste.sh
+++ cloud/deploy_paste.sh
@@ -14,7 +14,6 @@ chmod 400 "$KEY"
 
 export DOCKER_BUILDKIT=1
 RAW_IMAGE=image.tar
-PROJ=oracle
 REMOTE_USER=ubuntu
 PRIVATE_KEY="$KEY"
 PUBLIC_IP_PASTE="oracle1.anurag.sh"
@@ -23,10 +22,6 @@ SSH_COMMAND="ssh -q -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i
 set -x
 
 transfer_paste() {
-    (cd ../servers/oracle1 && docker-compose -p "$PROJ" build)
-    docker save -o "$RAW_IMAGE" "${PROJ}_frontend" "${PROJ}_paste" "${PROJ}_redis"
-    zstd -T0 -20 --ultra --rsyncable "$RAW_IMAGE" -o "$RAW_IMAGE.zst"
-
     rsync \
         --delete \
         -avz \
@@ -35,15 +30,6 @@ transfer_paste() {
         ./$RAW_IMAGE.zst \
         "../servers/oracle1/docker-compose.yml" \
         "$REMOTE_USER@$PUBLIC_IP_PASTE:/home/$REMOTE_USER/app"
-
-    $SSH_COMMAND "$REMOTE_USER@$PUBLIC_IP_PASTE" "bash -s" << EOF
-    set -x
-    cd app
-    zstd -f -d -T0 "$RAW_IMAGE".zst -o "$RAW_IMAGE"
-    docker load -i "$RAW_IMAGE"
-    docker-compose -p "$PROJ" up -d --no-build
-    docker ps
-EOF
 }
 
 transfer_paste
