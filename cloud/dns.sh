5534387097029641aff64a7f0e228e96b8de8146
Anurag Singh <contact@anurag.sh>
2022-02-26 19:07:21 +0800

    fix

--- cloud/dns.sh
+++ cloud/dns.sh
@@ -6,4 +6,4 @@ cfcli -d 'anurag.sh' rm ww3
 cfcli -t A -d 'anurag.sh' add oracle1 "$(terraform output -raw public-ip-paste)"
 cfcli -t A -d 'anurag.sh' add oracle2 "$(terraform output -raw public-ip-buildkite)"
 
-cfcli -t CNAME -d 'anurag.sh' add ww3 oracle1.anurag.sh
+cfcli -t CNAME -d 'anurag.sh' -a add ww3 oracle1.anurag.sh
