843a4e060c461b91cb2493fe6c1ff0660f257c0c
Anurag Singh <contact@anurag.sh>
2022-02-26 18:58:44 +0800

    remove paste

--- cloud/dns.sh
+++ cloud/dns.sh
@@ -2,11 +2,7 @@
 cfcli -d 'anurag.sh' rm oracle1
 cfcli -d 'anurag.sh' rm oracle2
 
-cfcli -d 'anurag.sh' rm files
-cfcli -d 'anurag.sh' rm paste
-
 cfcli -t A -d 'anurag.sh' add oracle1 "$(terraform output -raw public-ip-paste)"
 cfcli -t A -d 'anurag.sh' add oracle2 "$(terraform output -raw public-ip-buildkite)"
 
-cfcli -t CNAME -d 'anurag.sh' add files oracle2.anurag.sh
-cfcli -t CNAME -d 'anurag.sh' -a add paste oracle1.anurag.sh
+cfcli -t CNAME -d 'anurag.sh' add ww3 oracle1.anurag.sh
