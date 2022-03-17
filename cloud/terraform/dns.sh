1767a2ddc86d41681023e0daa7ad6beeeb7c0cdc
Anurag Singh <contact@anurag.sh>
2022-03-17 22:20:23 +0800

    rm dns

--- cloud/terraform/dns.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cfcli -d 'anurag.sh' rm oracle1
-cfcli -d 'anurag.sh' rm oracle2
-cfcli -d 'anurag.sh' rm ww3
-
-cfcli -t A -d 'anurag.sh' add oracle1 "$(terraform output -raw public-ip-paste)"
-cfcli -t A -d 'anurag.sh' add oracle2 "$(terraform output -raw public-ip-buildkite)"
-
-cfcli -t CNAME -d 'anurag.sh' -a add ww3 oracle1.anurag.sh
