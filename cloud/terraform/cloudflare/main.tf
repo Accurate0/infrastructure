fa7e3de65b281eff4350d1329601e0a6b72dfb36
Anurag Singh <contact@anurag.sh>
2022-03-17 22:24:12 +0800

    split file

--- cloud/terraform/cloudflare/main.tf
+++ cloud/terraform/cloudflare/main.tf
@@ -16,21 +16,3 @@ terraform {
 
 
 provider "cloudflare" {}
-
-resource "cloudflare_record" "root" {
-  zone_id         = var.cloudflare_zone_id
-  name            = "@"
-  value           = "accurate0.github.io"
-  type            = "CNAME"
-  ttl             = 1
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "www" {
-  zone_id         = var.cloudflare_zone_id
-  name            = "www"
-  value           = "anurag.sh"
-  type            = "CNAME"
-  ttl             = 1
-  allow_overwrite = true
-}
