06889e08148d258f329118d43734f8c8dcff994e
Anurag Singh <contact@anurag.sh>
2022-03-21 21:06:54 +0800

    I DONT WANT TO PAY FOR LOGS

--- ww3-api/api.tf
+++ ww3-api/api.tf
@@ -9,11 +9,29 @@ resource "aws_apigatewayv2_stage" "ww3-api" {
   name        = "ww3-api"
   auto_deploy = true
 
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api-gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+
   default_route_settings {
     throttling_burst_limit = 10
     throttling_rate_limit  = 10
+    logging_level          = "OFF"
   }
-
 }
 
 resource "aws_apigatewayv2_integration" "status" {
@@ -31,11 +49,17 @@ resource "aws_apigatewayv2_route" "status" {
   target    = "integrations/${aws_apigatewayv2_integration.status.id}"
 }
 
-resource "aws_lambda_permission" "api_gw" {
+resource "aws_lambda_permission" "api-gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ww3-api.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.ww3-api.execution_arn}/*/*"
+}
+
+resource "aws_cloudwatch_log_group" "api-gw" {
+  name = "/aws/api-gw/${aws_apigatewayv2_api.ww3-api.name}"
+
+  retention_in_days = 30
 }
