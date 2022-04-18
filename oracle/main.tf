68952b03b8d13279a756c3c9c63ccdf0b8bcdc5e
Anurag Singh <contact@anurag.sh>
2022-04-18 20:31:27 +0800

    Revert "move to oracle/oci"
    This reverts commit 46189abbc88399fefa560a3b79b144fa17a583c3.

--- oracle/main.tf
+++ oracle/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     oci = {
-      source  = "oracle/oci"
-      version = "4.71.0"
+      source  = "hashicorp/oci"
+      version = "4.33.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
