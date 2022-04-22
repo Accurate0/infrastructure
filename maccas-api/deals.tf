33adb3defc86b615f226f7f38b0b62c730994c51
Anurag Singh <contact@anurag.sh>
2022-04-22 18:32:43 +0800

    deploy

--- maccas-api/deals.tf
+++ maccas-api/deals.tf
@@ -92,7 +92,6 @@ resource "aws_api_gateway_integration" "code-get-api-integration" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
-
 resource "aws_api_gateway_integration" "deals-refresh-api-integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.deals-refresh-post-api-resource.id
