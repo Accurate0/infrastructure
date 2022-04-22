6cf069651dfdd54fca944c23b0fb7383364c8818
Anurag Singh <contact@anurag.sh>
2022-04-22 16:11:53 +0800

    add refresh

--- maccas-api/deals.tf
+++ maccas-api/deals.tf
@@ -57,6 +57,14 @@ resource "aws_api_gateway_method" "deals-delete-api-method" {
   authorization    = "NONE"
 }
 
+resource "aws_api_gateway_method" "deals-refresh-api-method" {
+  rest_api_id      = aws_api_gateway_rest_api.api.id
+  resource_id      = aws_api_gateway_resource.deals-refresh-post-api-resource.id
+  http_method      = "POST"
+  api_key_required = true
+  authorization    = "NONE"
+}
+
 resource "aws_api_gateway_integration" "deals-post-api-integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.deals-dealid-post-api-resource.id
@@ -84,10 +92,25 @@ resource "aws_api_gateway_integration" "code-get-api-integration" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
+
+resource "aws_api_gateway_integration" "deals-refresh-api-integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.deals-refresh-post-api-resource.id
+  http_method             = aws_api_gateway_method.deals-refresh-api-method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api-deals.invoke_arn
+}
 resource "aws_api_gateway_resource" "deals-dealid-post-api-resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.deals-api-resource.id
   path_part   = "{dealId}"
+}
+
+resource "aws_api_gateway_resource" "deals-refresh-post-api-resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.deals-api-resource.id
+  path_part   = "refresh"
 }
 
 resource "aws_api_gateway_resource" "code-dealid-get-api-resource" {
