1c8e3626ebab3b1a2af52871e6b8ccc19e89ed20
Anurag Singh <contact@anurag.sh>
2022-04-30 00:22:29 +0800

    increase again, safer

--- maccas-api/lambda.tf
+++ maccas-api/lambda.tf
@@ -107,7 +107,7 @@ resource "aws_lambda_function" "api-refresh" {
   handler       = "bootstrap"
   role          = aws_iam_role.iam.arn
   filename      = data.archive_file.dummy.output_path
-  timeout       = 60
+  timeout       = 120
   memory_size   = 128
   runtime       = "provided.al2"
 }
