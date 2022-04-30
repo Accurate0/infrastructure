a1c28e912c25cb57db938509c3d02cb58ca48677
Anurag Singh <contact@anurag.sh>
2022-04-30 18:55:02 +0800

    update timeouts

--- maccas-api/singapore.tf
+++ maccas-api/singapore.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "api-refresh-singapore" {
   handler       = "bootstrap"
   role          = aws_iam_role.iam.arn
   filename      = data.archive_file.dummy.output_path
-  timeout       = 240
+  timeout       = 60
   memory_size   = 256
   runtime       = "provided.al2"
 }
