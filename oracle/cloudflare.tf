811501fec052c8afdb7c9b9571154ab633f7a9c3
Anurag Singh <contact@anurag.sh>
2022-05-15 15:35:23 +0800

    move

--- oracle/cloudflare.tf
+++ oracle/cloudflare.tf
@@ -16,16 +16,6 @@ resource "cloudflare_record" "oracle2" {
   allow_overwrite = true
 }
 
-resource "cloudflare_record" "ww3" {
-  zone_id         = var.cloudflare_zone_id
-  name            = "ww3"
-  value           = cloudflare_record.oracle1.hostname
-  type            = "CNAME"
-  ttl             = 1
-  proxied         = true
-  allow_overwrite = true
-}
-
 variable "cloudflare_zone_id" {
   type      = string
   sensitive = true
