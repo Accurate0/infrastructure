53b21853f38ea3d7ae1ccb39893adab96b8df46e
Anurag Singh <contact@anurag.sh>
2022-05-28 22:32:36 +0800

    rm unused tables (money)

--- maccas-api-v2/db.tf
+++ maccas-api-v2/db.tf
@@ -1,41 +1,17 @@
-resource "aws_dynamodb_table" "maccas-api-db" {
-  name           = "MaccasApiDb-v2"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "account_name"
-
-  attribute {
-    name = "account_name"
-    type = "S"
-  }
-}
-
 resource "aws_dynamodb_table" "maccas-api-cache-db" {
   name           = "MaccasApiCache-v2"
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
-  hash_key       = "account_name"
+  hash_key       = "deal_uuid"
 
-  attribute {
-    name = "account_name"
-    type = "S"
-  }
-}
-
-resource "aws_dynamodb_table" "maccas-api-offer-id-db" {
-  name           = "MaccasApiOfferId-v2"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "offer_id"
   ttl {
     enabled        = true
     attribute_name = "ttl"
   }
+
   attribute {
-    name = "offer_id"
+    name = "deal_uuid"
     type = "S"
   }
 }
