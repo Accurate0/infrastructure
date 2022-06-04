0ff95cccac570f2d12d5de708f1a112c7d3a226f
Anurag Singh <contact@anurag.sh>
2022-06-04 22:16:07 +0800

    add public product for access

--- maccas-api-v2/product.tf
+++ maccas-api-v2/product.tf
@@ -11,29 +11,43 @@ resource "azurerm_api_management_product_api" "maccas-api" {
   resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
 }
 
+resource "azurerm_api_management_product" "maccas-web-api-public" {
+  product_id          = "MaccasApiPublic"
+  display_name        = "Maccas Web API (public)"
+  published           = true
+  api_management_name = data.azurerm_api_management.general-apim.name
+  resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
+}
+
+resource "azurerm_api_management_product_api" "maccas-web-api" {
+  api_name            = azurerm_api_management_api.maccas-v2.name
+  product_id          = azurerm_api_management_product.maccas-web-api-public.product_id
+  api_management_name = data.azurerm_api_management.general-apim.name
+  resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
+}
+
+resource "azurerm_api_management_product_policy" "maccas-web-api-public" {
+  product_id          = azurerm_api_management_product.maccas-web-api-public.product_id
+  api_management_name = azurerm_api_management_product.maccas-web-api-public.api_management_name
+  resource_group_name = azurerm_api_management_product.maccas-web-api-public.resource_group_name
+
+  xml_content = file("policy/public.policy.xml")
+}
+
 resource "azurerm_api_management_subscription" "maccas-v2-subscription" {
   api_management_name = data.azurerm_api_management.general-apim.name
   resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
-  api_id              = azurerm_api_management_api.maccas-v2.id
+  product_id          = azurerm_api_management_product.maccas-web-api-public.id
   display_name        = "Maccas Web API (public)"
   state               = "active"
   allow_tracing       = false
-}
-
-resource "azurerm_api_management_named_value" "maccas-api-public-id" {
-  name                = "maccas-api-public-id"
-  resource_group_name = data.azurerm_resource_group.general-api-group.name
-  api_management_name = data.azurerm_api_management.general-apim.name
-  display_name        = "MaccasApiPublicId"
-  secret              = true
-  value               = element(split("/", azurerm_api_management_subscription.maccas-v2-subscription.id), length(split("/", azurerm_api_management_subscription.maccas-v2-subscription.id)) - 1)
 }
 
 resource "azurerm_api_management_product" "maccas-policy-apim" {
   product_id            = "MaccasApiKey"
   api_management_name   = data.azurerm_api_management.general-apim.name
   resource_group_name   = data.azurerm_api_management.general-apim.resource_group_name
-  display_name          = "Maccas API Key"
+  display_name          = "Maccas API Key (API Usage)"
   subscription_required = true
   published             = true
 }
@@ -42,6 +56,6 @@ resource "azurerm_api_management_subscription" "maccas-policy-apim-subscription"
   api_management_name = data.azurerm_api_management.general-apim.name
   resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
   product_id          = azurerm_api_management_product.maccas-policy-apim.id
-  display_name        = "MaccasApiKey"
+  display_name        = "Maccas API Key (API Usage)"
   state               = "active"
 }
