cf3ca5412a5d87d4bc9ca2d9e29511af62947ac3
Anurag Singh <contact@anurag.sh>
2022-06-11 15:57:27 +0800

    remove refresh

--- maccas-api/cloudwatch.tf
+++ maccas-api/cloudwatch.tf
@@ -7,8 +7,3 @@ resource "aws_cloudwatch_log_group" "api-deals-log" {
   name              = "/aws/lambda/${aws_lambda_function.api-deals.function_name}"
   retention_in_days = 14
 }
-
-resource "aws_cloudwatch_log_group" "api-refresh-log" {
-  name              = "/aws/lambda/${aws_lambda_function.api-refresh.function_name}"
-  retention_in_days = 14
-}
--- maccas-api/lambda.tf
+++ maccas-api/lambda.tf
@@ -101,18 +101,3 @@ resource "aws_lambda_function" "api-deals" {
   memory_size   = 128
   runtime       = "provided.al2"
 }
-
-resource "aws_lambda_function" "api-refresh" {
-  function_name = "MaccasApi-refresh"
-  handler       = "bootstrap"
-  role          = aws_iam_role.iam.arn
-  filename      = data.archive_file.dummy.output_path
-  timeout       = 120
-  memory_size   = 256
-  runtime       = "provided.al2"
-}
-
-# resource "aws_lambda_function_url" "lambda-url" {
-#   function_name      = aws_lambda_function.api-refresh.function_name
-#   authorization_type = "NONE"
-# }
