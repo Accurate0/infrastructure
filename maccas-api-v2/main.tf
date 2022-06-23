ba80633eda164f140cbd62dffd11c13e2bdaba0c
Anurag Singh <contact@anurag.sh>
2022-06-23 22:14:28 +0800

    add vercel

--- maccas-api-v2/main.tf
+++ maccas-api-v2/main.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "3.10.1"
     }
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 0.3"
+    }
   }
 
   cloud {
--- maccas-api-v2/static-site.tf
+++ maccas-api-v2/static-site.tf
@@ -1,30 +1,8 @@
-resource "azurerm_static_site" "maccas-web" {
-  name                = "Maccas-Web"
-  resource_group_name = azurerm_resource_group.maccas-api.name
-  location            = "eastasia"
-}
-
-resource "azurerm_static_site_custom_domain" "maccas-web-domain" {
-  static_site_id  = azurerm_static_site.maccas-web.id
-  domain_name     = "maccas.anurag.sh"
-  validation_type = "dns-txt-token"
-}
-
 resource "cloudflare_record" "maccas" {
   zone_id         = var.cloudflare_zone_id
   name            = "maccas"
-  value           = azurerm_static_site.maccas-web.default_host_name
+  value           = "cname.vercel-dns.com"
   type            = "CNAME"
-  proxied         = false
-  ttl             = 1
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "maccas-txt" {
-  zone_id         = var.cloudflare_zone_id
-  name            = "maccas"
-  value           = azurerm_static_site_custom_domain.maccas-web-domain.validation_token
-  type            = "TXT"
   proxied         = false
   ttl             = 1
   allow_overwrite = true
@@ -33,4 +11,18 @@ resource "cloudflare_record" "maccas-txt" {
 variable "cloudflare_zone_id" {
   type      = string
   sensitive = true
+}
+
+resource "vercel_project" "maccas-web" {
+  name      = "maccas-web"
+  framework = "nextjs"
+  git_repository = {
+    type = "github"
+    repo = "Accurate0/maccas-web"
+  }
+}
+
+resource "vercel_project_domain" "maccas-web-domain" {
+  project_id = vercel_project.maccas-web.id
+  domain     = "maccas.anurag.sh"
 }
