822b7476420433542e9be37590ee7036b07b2ca1
Anurag Singh <contact@anurag.sh>
2022-06-30 17:24:22 +0800

    remove from key

--- kvp-api/product.tf
+++ kvp-api/product.tf
@@ -4,22 +4,9 @@ data "azurerm_api_management_product" "maccas-bot" {
   resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
 }
 
-data "azurerm_api_management_product" "maccas-apim" {
-  product_id          = "MaccasApiKey"
-  api_management_name = data.azurerm_api_management.general-apim.name
-  resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
-}
-
 resource "azurerm_api_management_product_api" "kvp-api" {
   api_name            = azurerm_api_management_api.kvp-v1.name
   product_id          = data.azurerm_api_management_product.maccas-bot.product_id
-  api_management_name = data.azurerm_api_management.general-apim.name
-  resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
-}
-
-resource "azurerm_api_management_product_api" "kvp-api-2" {
-  api_name            = azurerm_api_management_api.kvp-v1.name
-  product_id          = data.azurerm_api_management_product.maccas-apim.product_id
   api_management_name = data.azurerm_api_management.general-apim.name
   resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
 }
