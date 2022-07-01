b57176a298525bc63448e10517c16481388d3775
Anurag Singh <contact@anurag.sh>
2022-07-01 21:00:00 +0800

    remove bot product

--- places-api/product.tf
+++ places-api/product.tf
@@ -1,18 +1,5 @@
-data "azurerm_api_management_product" "maccas-bot" {
-  product_id          = "MaccasBot"
-  api_management_name = data.azurerm_api_management.general-apim.name
-  resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
-}
-
 data "azurerm_api_management_product" "maccas-apim" {
   product_id          = "MaccasApiKey"
-  api_management_name = data.azurerm_api_management.general-apim.name
-  resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
-}
-
-resource "azurerm_api_management_product_api" "places-api" {
-  api_name            = azurerm_api_management_api.places-v1.name
-  product_id          = data.azurerm_api_management_product.maccas-bot.product_id
   api_management_name = data.azurerm_api_management.general-apim.name
   resource_group_name = data.azurerm_api_management.general-apim.resource_group_name
 }
