acc5d709516ee42a68737da31ee0791457bae0fa
Anurag Singh <contact@anurag.sh>
2022-07-06 18:03:18 +0800

    update stage name

--- maccas-api/deals.tf
+++ maccas-api/deals.tf
@@ -1,7 +1,7 @@
 resource "aws_api_gateway_stage" "api-stage" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.api-deployment.id
-  stage_name    = "v2"
+  stage_name    = "v1"
 }
 
 resource "aws_api_gateway_method" "locations-api-method" {
