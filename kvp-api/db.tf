8a0634eba2407f815f3e11198035d36b20291efc
Anurag Singh <contact@anurag.sh>
2022-07-14 19:30:30 +0800

    lower limit

--- kvp-api/db.tf
+++ kvp-api/db.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "kvp-api-db" {
   name           = "KvpApiDb"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
+  read_capacity  = 1
+  write_capacity = 1
   hash_key       = "key"
 
   attribute {
