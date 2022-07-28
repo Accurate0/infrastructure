b8722121ef49666656877c4bfe1cbbb5dceb8025
Anurag Singh <contact@anurag.sh>
2022-07-28 21:37:38 +0800

    rm other regions

--- maccas-api/regions.tf
+++ maccas-api/regions.tf
@@ -9,15 +9,3 @@ module "refresh-sng" {
   role_arn = aws_iam_role.iam.arn
   region   = "ap-southeast-1"
 }
-
-module "refresh-tok" {
-  source   = "./module/refresh-regional-lambda"
-  role_arn = aws_iam_role.iam.arn
-  region   = "ap-northeast-1"
-}
-
-module "refresh-seo" {
-  source   = "./module/refresh-regional-lambda"
-  role_arn = aws_iam_role.iam.arn
-  region   = "ap-northeast-2"
-}
