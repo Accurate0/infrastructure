9b9bf77aad302caae644fd8c388cf5e2512af8b3
Anurag Singh <contact@anurag.sh>
2022-08-02 17:47:30 +0800

    rm ext value

--- light-api/keys.tf
+++ light-api/keys.tf
@@ -6,16 +6,14 @@ resource "azurerm_api_management_subscription" "light-subscription" {
   state               = "active"
 }
 
-variable "home_assistant_api_key" {
-  type      = string
-  sensitive = true
-}
-
 resource "azurerm_api_management_named_value" "home-assistant-api-key" {
   name                = "home-assistant-api-key"
   resource_group_name = data.azurerm_resource_group.general-api-group.name
   api_management_name = data.azurerm_api_management.general-apim.name
   display_name        = "HomeAssistantApiKey"
   secret              = true
-  value               = var.home_assistant_api_key
+  value               = "VALUE_REPLACED_IN_PORTAL"
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
