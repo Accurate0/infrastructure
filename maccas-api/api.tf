65bc9bcf0541e233dfa005aa9a838c9a25a2086b
Anurag Singh <contact@anurag.sh>
2022-08-18 19:55:07 +0800

    use new module

--- maccas-api/api.tf
+++ maccas-api/api.tf
@@ -6,44 +6,16 @@ resource "aws_api_gateway_rest_api" "api" {
   }
 }
 
+resource "aws_api_gateway_stage" "api-stage" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.api-deployment.id
+  stage_name    = "v1"
+}
+
 resource "aws_api_gateway_deployment" "api-deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   depends_on = [
     aws_api_gateway_rest_api.api,
-
-    aws_api_gateway_resource.deals-api-resource,
-    aws_api_gateway_resource.code-api-resource,
-    aws_api_gateway_resource.code-dealid-get-api-resource,
-    aws_api_gateway_resource.deals-dealid-post-api-resource,
-    aws_api_gateway_resource.locations-api-resource,
-    aws_api_gateway_resource.lock-api-resource,
-    aws_api_gateway_resource.users-api-resource,
-    aws_api_gateway_resource.users-config-api-resource,
-    aws_api_gateway_resource.locations-search-api-resource,
-    aws_api_gateway_resource.last-refresh-api-resource,
-
-    aws_api_gateway_method.deals-api-method,
-    aws_api_gateway_method.code-api-method,
-    aws_api_gateway_method.deals-delete-api-method,
-    aws_api_gateway_method.deals-post-api-method,
-    aws_api_gateway_method.locations-api-method,
-    aws_api_gateway_method.lock-post-api-method,
-    aws_api_gateway_method.lock-delete-api-method,
-    aws_api_gateway_method.locations-search-api-method,
-    aws_api_gateway_method.user-config-get-api-method,
-    aws_api_gateway_method.user-config-post-api-method,
-    aws_api_gateway_method.last-refresh-api-method,
-
-    aws_api_gateway_integration.deals-api-integration,
-    aws_api_gateway_integration.deals-delete-api-integration,
-    aws_api_gateway_integration.code-get-api-integration,
-    aws_api_gateway_integration.locations-get-api-integration,
-    aws_api_gateway_integration.lock-post-api-integration,
-    aws_api_gateway_integration.lock-delete-api-integration,
-    aws_api_gateway_integration.locations-search-post-api-integration,
-    aws_api_gateway_integration.user-config-get-api-integration,
-    aws_api_gateway_integration.user-config-post-api-integration,
-    aws_api_gateway_integration.last-refresh-api-integration,
 
     module.statistics,
     module.account,
@@ -54,10 +26,19 @@ resource "aws_api_gateway_deployment" "api-deployment" {
     module.accountId,
     module.docs,
     module.openapi,
+    module.deals,
+    module.deals-dealid,
+    module.code,
+    module.code-dealid,
+    module.locations,
+    module.locations-search,
+    module.deals-last-refresh,
+    module.user,
+    module.user-config
   ]
 
   triggers = {
-    redeployment = jsonencode([filesha1("deals.tf"), filesha1("api.tf"), filesha1("endpoints.tf")])
+    redeployment = jsonencode([filesha1("api.tf"), filesha1("endpoints.tf")])
   }
 
   lifecycle {
--- maccas-api/endpoints.tf
+++ maccas-api/endpoints.tf
@@ -133,3 +133,158 @@ module "openapi" {
     }
   ]
 }
+
+
+module "code" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = aws_api_gateway_rest_api.api.root_resource_id
+  resource         = "code"
+  cors             = false
+  methods          = []
+}
+
+
+module "code-dealid" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = module.code.resource
+  resource         = "{dealId}"
+  cors             = false
+  methods = [
+    {
+      method     = "GET"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    }
+  ]
+}
+
+
+module "deals" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = aws_api_gateway_rest_api.api.root_resource_id
+  resource         = "deals"
+  cors             = false
+  methods = [
+    {
+      method     = "GET"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    }
+  ]
+}
+
+module "deals-last-refresh" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = module.deals.resource
+  resource         = "last-refresh"
+  cors             = false
+  methods = [
+    {
+      method     = "GET"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    }
+  ]
+}
+
+
+module "deals-dealid" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = module.deals.resource
+  resource         = "{dealId}"
+  cors             = false
+  methods = [
+    {
+      method     = "POST"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    },
+    {
+      method     = "DELETE"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    }
+  ]
+}
+
+module "locations" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = aws_api_gateway_rest_api.api.root_resource_id
+  resource         = "locations"
+  cors             = false
+  methods = [
+    {
+      method     = "GET"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    }
+  ]
+}
+
+module "locations-search" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = module.locations.resource
+  resource         = "search"
+  cors             = false
+  methods = [
+    {
+      method     = "GET"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    }
+  ]
+}
+
+module "user" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = aws_api_gateway_rest_api.api.root_resource_id
+  resource         = "user"
+  cors             = false
+  methods          = []
+}
+
+module "user-config" {
+  source           = "Accurate0/serverless-resource/aws"
+  version          = "2.1.0"
+  api_key_required = true
+  api              = aws_api_gateway_rest_api.api.id
+  root_resource    = module.user.resource
+  resource         = "config"
+  cors             = false
+  methods = [
+    {
+      method     = "GET"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    },
+    {
+      method     = "POST"
+      type       = null
+      invoke_arn = aws_lambda_function.api.invoke_arn
+    }
+  ]
+}
