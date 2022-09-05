5969ff65fd6f11f39f6410833dbe9da567b52848
Anurag Singh <contact@anurag.sh>
2022-09-05 22:48:53 +0800

    add log

--- maccas-api/cloudwatch.tf
+++ maccas-api/cloudwatch.tf
@@ -7,3 +7,8 @@ resource "aws_cloudwatch_log_group" "cleanup-log" {
   name              = "/aws/lambda/${aws_lambda_function.cleanup.function_name}"
   retention_in_days = 14
 }
+
+resource "aws_cloudwatch_log_group" "accounts-log" {
+  name              = "/aws/lambda/${aws_lambda_function.accounts.function_name}"
+  retention_in_days = 14
+}
