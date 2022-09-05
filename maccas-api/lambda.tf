970961c2f2de3b996464598b955dc74b509de139
Anurag Singh <contact@anurag.sh>
2022-09-05 20:58:04 +0800

    add new queue and lambda

--- maccas-api/lambda.tf
+++ maccas-api/lambda.tf
@@ -82,7 +82,10 @@ resource "aws_iam_policy" "resource-access" {
           "sqs:GetQueueAttributes",
           "sqs:GetQueueUrl"
         ],
-        "Resource" = ["${aws_sqs_queue.maccas-cleanup-queue.arn}"]
+        "Resource" = [
+            "${aws_sqs_queue.maccas-cleanup-queue.arn}",
+            "${aws_sqs_queue.maccas-accounts-queue.arn}"
+        ]
       }
     ]
   })
@@ -120,6 +123,16 @@ resource "aws_lambda_function" "api" {
 
 resource "aws_lambda_function" "cleanup" {
   function_name = "MaccasApi-cleanup"
+  handler       = "bootstrap"
+  role          = aws_iam_role.iam.arn
+  filename      = data.archive_file.dummy.output_path
+  timeout       = 15
+  memory_size   = 128
+  runtime       = "provided.al2"
+}
+
+resource "aws_lambda_function" "accounts" {
+  function_name = "MaccasApi-accounts"
   handler       = "bootstrap"
   role          = aws_iam_role.iam.arn
   filename      = data.archive_file.dummy.output_path
--- maccas-api/queue.tf
+++ maccas-api/queue.tf
@@ -7,9 +7,27 @@ resource "aws_sqs_queue" "maccas-cleanup-queue" {
   sqs_managed_sse_enabled   = true
 
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.maccas-cleanup-dlq.arn
+    deadLetterTargetArn = aws_sqs_queue.maccas-dlq.arn
     maxReceiveCount     = 2
   })
+}
+
+resource "aws_sqs_queue" "maccas-accounts-queue" {
+  name                      = "maccas-accounts-queue"
+  delay_seconds             = 600
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.maccas-dlq.arn
+    maxReceiveCount     = 2
+  })
+}
+
+resource "aws_sqs_queue" "maccas-dlq" {
+  name = "maccas-dlq"
 }
 
 resource "aws_lambda_event_source_mapping" "maccas-cleanup-event-mapping" {
@@ -19,6 +37,9 @@ resource "aws_lambda_event_source_mapping" "maccas-cleanup-event-mapping" {
   batch_size       = 1
 }
 
-resource "aws_sqs_queue" "maccas-cleanup-dlq" {
-  name = "maccas-cleanup-dlq"
+resource "aws_lambda_event_source_mapping" "maccas-accounts-event-mapping" {
+  event_source_arn = aws_sqs_queue.maccas-accounts-queue.arn
+  enabled          = true
+  function_name    = aws_lambda_function.accounts.function_name
+  batch_size       = 1
 }
