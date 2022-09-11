8ed7c1dcc8fe4fccf0420b9edba9f8239bf2cd30
Anurag Singh <contact@anurag.sh>
2022-09-11 20:41:09 +0800

    change val

--- maccas-api/queue.tf
+++ maccas-api/queue.tf
@@ -29,7 +29,7 @@ resource "aws_sqs_queue" "maccas-accounts-queue" {
 resource "aws_sqs_queue" "maccas-images-queue" {
   name                      = "maccas-images-queue"
   delay_seconds             = 0
-  max_message_size          = 2048
+  max_message_size          = 20480
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
   sqs_managed_sse_enabled   = true
