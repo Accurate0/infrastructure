847b55d9a09156a578848b8f1abda1c87d34849c
Anurag Singh <contact@anurag.sh>
2022-10-28 23:35:33 +0800

    update retention

--- azure/application-insights.tf
+++ azure/application-insights.tf
@@ -4,7 +4,7 @@ resource "azurerm_application_insights" "general-ai" {
   location             = azurerm_resource_group.general-api-group.location
   resource_group_name  = azurerm_resource_group.general-api-group.name
   application_type     = "other"
-  retention_in_days    = 30
+  retention_in_days    = 365
   daily_data_cap_in_gb = 0.166
   disable_ip_masking   = true
 }
