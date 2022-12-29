30b59618dd64fe763a074ffd85efd736f3232477
Anurag Singh <contact@anurag.sh>
2022-12-29 21:01:52 +0800

    rm unused

--- azure/main.tf
+++ azure/main.tf
@@ -21,22 +21,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-variable "ARM_B2C_CLIENT_ID" {
-  type = string
-}
-
-variable "ARM_B2C_CLIENT_SECRET" {
-  type = string
-}
-
-variable "ARM_B2C_TENANT_ID" {
-  type = string
-}
-
-provider "azurerm" {
-  alias         = "b2c"
-  client_id     = var.ARM_B2C_CLIENT_ID
-  client_secret = var.ARM_B2C_CLIENT_SECRET
-  tenant_id     = var.ARM_B2C_TENANT_ID
-}
