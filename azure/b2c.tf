80b091034682db596633910a67707b90acd37fb7
Anurag Singh <contact@anurag.sh>
2022-12-29 21:13:41 +0800

    update rg name

--- azure/b2c.tf
+++ azure/b2c.tf
@@ -1,6 +1,6 @@
 resource "azurerm_aadb2c_directory" "b2c" {
   data_residency_location = "Australia"
   domain_name             = "apib2clogin.onmicrosoft.com"
-  resource_group_name     = "general-api-group"
+  resource_group_name     = azurerm_resource_group.general-api-group.name
   sku_name                = "PremiumP1"
 }
