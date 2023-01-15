882bf36db1a9ea425d8f2390c1bd11ee73230281
Anurag Singh <contact@anurag.sh>
2023-01-15 15:03:49 +0800

    audience as secret et c

--- maccas-api/apim-named-values.tf
+++ maccas-api/apim-named-values.tf
@@ -34,6 +34,14 @@ resource "azurerm_api_management_named_value" "maccas-apim-jwt-bypass" {
   value               = base64encode(random_password.jwt-bypass-token.result)
 }
 
+resource "azurerm_api_management_named_value" "maccas-ad-client-id" {
+  name                = "maccas-ad-client-id"
+  resource_group_name = data.azurerm_resource_group.general-api-group.name
+  api_management_name = data.azurerm_api_management.general-apim.name
+  display_name        = "MaccasAdClientId"
+  value               = azuread_application.this.application_id
+}
+
 resource "random_password" "jwt-bypass-token" {
   length  = 128
   special = true
--- maccas-api/secrets.tf
+++ maccas-api/secrets.tf
@@ -15,3 +15,12 @@ resource "aws_secretsmanager_secret_version" "api-secret-jwt-bypass" {
   secret_id     = aws_secretsmanager_secret.api-secret-jwt-bypass.id
   secret_string = base64encode(random_password.jwt-bypass-token.result)
 }
+
+resource "aws_secretsmanager_secret" "api-secret-application-audience" {
+  name = "MaccasApi-ApplicationAudience"
+}
+
+resource "aws_secretsmanager_secret_version" "api-secret-application-audience" {
+  secret_id     = aws_secretsmanager_secret.api-secret-application-audience.id
+  secret_string = azuread_application.this.application_id
+}
