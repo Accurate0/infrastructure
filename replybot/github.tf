8fe08140642fdcd040f23ffe0d6d08411bcae77e
Anurag Singh <contact@anurag.sh>
2023-03-05 15:38:01 +0800

    remove github

--- replybot/github.tf
+++
@@ -1,5 +0,0 @@
-resource "github_actions_secret" "api-key-secret" {
-  repository      = "replybot"
-  secret_name     = "API_KEY"
-  plaintext_value = azurerm_api_management_subscription.internal-policy-apim-subscription.primary_key
-}
