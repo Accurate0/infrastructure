9d55ca14cee0794b162a5af36131e56090b9c5e0
Anurag Singh <contact@anurag.sh>
2023-03-12 12:48:53 +0800

    reduce provisioning

--- replybot/tables.tf
+++ replybot/tables.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "replybot-interaction" {
   name           = "ReplybotInteraction"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
+  read_capacity  = 1
+  write_capacity = 1
   hash_key       = "hash"
   attribute {
     name = "hash"
