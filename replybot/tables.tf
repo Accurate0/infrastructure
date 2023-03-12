deacc5add06adc572592d3af21bf77b08412d7c9
Anurag Singh <contact@anurag.sh>
2023-03-12 12:49:23 +0800

    inc a bit

--- replybot/tables.tf
+++ replybot/tables.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "replybot-interaction" {
   name           = "ReplybotInteraction"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
+  read_capacity  = 3
+  write_capacity = 3
   hash_key       = "hash"
   attribute {
     name = "hash"
