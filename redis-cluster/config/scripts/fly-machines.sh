d8c603763879f8bc46bf36d0c25a59021359a495
Anurag Singh <contact@anurag.sh>
2023-05-14 20:43:05 +0800

    update script

--- redis-cluster/config/scripts/fly-machines.sh
+++ redis-cluster/config/scripts/fly-machines.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 fly machine update --metadata fly_platform_version=v2 "$FLY_MACHINE_ID_SYD" --yes
-fly machine update --metadata fly_platform_version=v2 "$FLY_MACHINE_ID_SYD_2" --yes
 fly machine update --metadata fly_platform_version=v2 "$FLY_MACHINE_ID_SIN" --yes
