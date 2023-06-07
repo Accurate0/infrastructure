d9ccb67b17c8c662f9d2808608b862381a400c89
Anurag Singh <contact@anurag.sh>
2023-06-07 21:04:20 +0800

    unused

--- vpn/network.tf
+++
@@ -1,3 +0,0 @@
-data "aws_subnet" "syda-subnet" {
-  id = "subnet-039fb3511186a9bc2"
-}
--- vpn/security-groups.tf
+++
@@ -1,8 +0,0 @@
-
-data "aws_security_group" "internal-sg" {
-  id = "sg-0e8b5f765739d0864"
-}
-
-data "aws_security_group" "internal-wireguard-sg" {
-  id = "sg-075ac12eb01310c30"
-}
