0da2d870c1b4a41e4f5af84d6e176bacf0814c6b
Anurag Singh <contact@anurag.sh>
2023-06-25 16:54:51 +0800

    rm unused apim

--- module/keyvault-value/main.tf
+++ module/keyvault-value/main.tf
@@ -11,11 +11,6 @@ data "azurerm_resource_group" "general-api-group" {
   name = "general-api-group"
 }
 
-data "azurerm_api_management" "general-apim" {
-  name                = "general-apim"
-  resource_group_name = data.azurerm_resource_group.general-api-group.name
-}
-
 data "azurerm_key_vault" "kv" {
   name                = "gen-shared-kv"
   resource_group_name = data.azurerm_resource_group.general-api-group.name
