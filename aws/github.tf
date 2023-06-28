f947a6803b115e612857dee18dfea44d5beb9d8a
Anurag Singh <contact@anurag.sh>
2023-06-28 18:03:23 +0800

    add 2nd thumbprint according to blog

--- aws/github.tf
+++ aws/github.tf
@@ -17,5 +17,9 @@ resource "aws_iam_openid_connect_provider" "github" {
     "sts.amazonaws.com",
   ]
 
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  # https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
 }
