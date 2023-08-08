840969bc5c688ae69e9dab6c907b7e7e7a450e2a
Anurag Singh <contact@anurag.sh>
2023-08-08 17:57:59 +0800

    delete unused

--- graph/keyvault-named-values.tf
+++
@@ -1,6 +0,0 @@
-module "b2c-client-secret" {
-  source       = "../module/keyvault-named-value"
-  named_value  = "B2CClientSecret"
-  secret_name  = "b2c-client-secret"
-  secret_value = azuread_application_password.this.value
-}
--- places/keyvault-named-value.tf
+++
@@ -1,6 +0,0 @@
-module "google-places-api-key" {
-  source       = "../module/keyvault-named-value"
-  named_value  = "GooglePlacesApiKey"
-  secret_name  = "google-places-api-key"
-  secret_value = google_apikeys_key.this.key_string
-}
