c22afe1a873852219d4f138e65336cbca4099a15
Anurag Singh <contact@anurag.sh>
2023-08-08 18:02:54 +0800

    yea

--- module/redis/main.tf
+++ module/redis/main.tf
@@ -1,19 +1,10 @@
 terraform {
   required_providers {
-    upstash = {
-      source  = "upstash/upstash"
-      version = ">= 1.3.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3"
     }
   }
-}
-
-provider "upstash" {
-  api_key = var.UPSTASH_API_KEY
-  email   = var.UPSTASH_EMAIL
 }
 
 provider "azurerm" {
--- module/redis/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "UPSTASH_API_KEY" {
-  type = string
-}
-
-variable "UPSTASH_EMAIL" {
-  type = string
-}
