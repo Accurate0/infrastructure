9a1be4fab07c75123d4fbcde43d3bf7fc34344f0
Anurag Singh <contact@anurag.sh>
2023-11-05 16:13:13 +0800

    add places (new)

--- maccas/google.tf
+++ maccas/google.tf
@@ -12,12 +12,29 @@ resource "google_apikeys_key" "this" {
       methods = []
       service = "places-backend.googleapis.com"
     }
+
+    api_targets {
+      methods = []
+      service = "places.googleapis.com"
+    }
   }
 }
 
 resource "google_project_service" "places-service" {
   project = data.google_project.places.project_id
   service = "places-backend.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "places-new-service" {
+  project = data.google_project.places.project_id
+  service = "places.googleapis.com"
 
   timeouts {
     create = "30m"
