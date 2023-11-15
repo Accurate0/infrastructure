eb2010b2e673b17a4d64c31be11ffa837b01eea9
Anurag Singh <contact@anurag.sh>
2023-11-15 16:32:03 +0800

    og

--- anurag-sh/site.tf
+++ anurag-sh/site.tf
@@ -8,6 +8,16 @@ resource "cloudflare_record" "root" {
   allow_overwrite = true
 }
 
+resource "cloudflare_record" "og" {
+  zone_id         = var.cloudflare_zone_id
+  name            = "og"
+  value           = "cname.vercel-dns.com"
+  type            = "CNAME"
+  proxied         = false
+  ttl             = 1
+  allow_overwrite = true
+}
+
 resource "cloudflare_record" "www" {
   zone_id         = var.cloudflare_zone_id
   name            = "www"
@@ -30,4 +40,21 @@ resource "vercel_project" "anurag-sh" {
 resource "vercel_project_domain" "anurag-sh-domain" {
   project_id = vercel_project.anurag-sh.id
   domain     = "anurag.sh"
+}
+
+resource "vercel_project" "og" {
+  name      = "og"
+  framework = "nextjs"
+  git_repository = {
+    type = "github"
+    repo = "Accurate0/vercel-og-nextjs"
+  }
+  vercel_authentication = {
+    protect_production = false
+  }
+}
+
+resource "vercel_project_domain" "og-domain" {
+  project_id = vercel_project.og.id
+  domain     = "og.anurag.sh"
 }
