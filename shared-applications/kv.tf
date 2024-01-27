51a8034d299088a1c80bfeb70754c367ed1ea8c6
Anurag Singh <contact@anurag.sh>
2024-01-28 00:31:22 +0800

    fix

--- shared-applications/kv.tf
+++
@@ -1,9 +0,0 @@
-module "umami-database-connection" {
-  source      = "../module/keyvault-value-output"
-  secret_name = "umami-database-connection"
-}
-
-module "umami-app-secret" {
-  source      = "../module/keyvault-value-output"
-  secret_name = "umami-app-secret"
-}
