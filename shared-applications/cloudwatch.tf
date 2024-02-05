1884184e9ab6f433c9cae963fcb0b03bc5aec3f2
Anurag Singh <contact@anurag.sh>
2024-02-05 22:04:20 +0800

    remove

--- shared-applications/cloudwatch.tf
+++ shared-applications/cloudwatch.tf
@@ -7,8 +7,3 @@ resource "aws_cloudwatch_log_group" "uptime" {
   name              = "/aws/ecs/uptime"
   retention_in_days = 14
 }
-
-resource "aws_cloudwatch_log_group" "umami" {
-  name              = "/aws/ecs/umami"
-  retention_in_days = 14
-}
