2eae1e34d2bf90e06514bc9283a499a9de3afe26
Anurag Singh <contact@anurag.sh>
2024-05-25 19:03:37 +0800

    remove

--- shared-applications/cloudflare.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "uptime" {
-  zone_id = var.cloudflare_zone_id
-  name    = "uptime"
-  value   = "worker.anurag.sh"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
--- shared-applications/ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "nginx" {
-  name = "nginx"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
