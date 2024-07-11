2dbab28bf26ced8d541f34d843a401f684c6c2e6
Anurag Singh <contact@anurag.sh>
2024-07-11 20:41:44 +0800

    fix secrets

--- ozb/cloudwatch.tf
+++ ozb/cloudwatch.tf
@@ -1,8 +1,3 @@
-resource "aws_cloudwatch_log_group" "this" {
-  name              = "/aws/ecs/ozb"
-  retention_in_days = 14
-}
-
 resource "aws_cloudwatch_log_group" "trigger-log" {
   name              = "/aws/lambda/${aws_lambda_function.trigger.function_name}"
   retention_in_days = 14
