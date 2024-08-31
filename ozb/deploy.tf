aea3ecaafa79edd0d429d09a3c0762f5670f36ac
Anurag Singh <contact@anurag.sh>
2024-08-31 15:41:13 +0800

    remove ecr

--- ozb/deploy.tf
+++ ozb/deploy.tf
@@ -6,27 +6,6 @@ module "aws-oidc-deploy" {
 
     "Statement" = [
       {
-        "Effect" = "Allow",
-        "Action" = [
-          "ecr:GetAuthorizationToken",
-        ],
-        "Resource" = [
-          "*",
-        ]
-      },
-      {
-        "Action" : [
-          "ecr:BatchGetImage",
-          "ecr:CompleteLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:InitiateLayerUpload",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:PutImage"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "${aws_ecr_repository.this.arn}"
-      },
-      {
         "Action" : [
           "lambda:UpdateFunctionCode",
         ],
@@ -40,10 +19,4 @@ module "aws-oidc-deploy" {
     ]
   }
   allowed_repos = ["ozb"]
-}
-
-resource "github_actions_variable" "ecr-registry-url" {
-  repository    = "ozb"
-  variable_name = "AWS_ECR_REGISTRY"
-  value         = aws_ecr_repository.this.repository_url
 }
--- replybot/ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "this" {
-  name = "replybot"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
