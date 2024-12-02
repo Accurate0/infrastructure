763d6f4c0af782628cbf0f638a46de110a0174dc
Anurag Singh <contact@anurag.sh>
2024-12-02 21:47:04 +0800

    add applications :)

--- github/github.tf
+++ github/github.tf
@@ -9,3 +9,10 @@ resource "github_actions_secret" "kubeconfig" {
   plaintext_value = module.kubeconfig.secret_value
 }
 
+resource "github_actions_secret" "argocd-actions-token" {
+  for_each        = local.repos
+  repository      = each.value
+  secret_name     = "ARGOCD_AUTH_TOKEN"
+  plaintext_value = module.argocd-actions-token.secret_value
+}
+
--- github/secrets.tf
+++ github/secrets.tf
@@ -3,3 +3,8 @@ module "kubeconfig" {
   secret_name = "kubeconfig"
 }
 
+module "argocd-actions-token" {
+  source      = "../module/keyvault-value-output"
+  secret_name = "argocd-actions-token"
+}
+
