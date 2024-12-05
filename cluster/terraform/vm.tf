1947af34372807d81a745960bb2fcafeff502306
Anurag Singh <contact@anurag.sh>
2024-12-05 20:47:58 +0800

    this is getting expensive...

--- cluster/terraform/vm.tf
+++ cluster/terraform/vm.tf
@@ -12,7 +12,7 @@ resource "binarylane_server" "control" {
 
 locals {
   control_count = 1
-  worker_count  = 2
+  worker_count  = 3
   proxy_count   = 1
 }
 
@@ -21,7 +21,7 @@ resource "binarylane_server" "worker" {
   image             = "ubuntu-24.04"
   name              = "k8s-worker-${count.index + 1}"
   region            = "per"
-  size              = "std-1vcpu"
+  size              = "std-2vcpu"
   port_blocking     = false
   public_ipv4_count = 1
   vpc_id            = binarylane_vpc.kubernetes-vpc.id
