896cf2f7ffbb6cada27d72aaff96170b128c5c34
Anurag Singh <contact@anurag.sh>
2024-12-07 18:55:01 +0800

    reduce server size

--- binarylane/vm.tf
+++ binarylane/vm.tf
@@ -2,7 +2,7 @@ resource "binarylane_server" "perth" {
   image             = "ubuntu-24.04"
   name              = "snake-street.bnr.la"
   region            = "per"
-  size              = "std-4vcpu"
+  size              = "std-2vcpu"
   port_blocking     = false
   vpc_id            = binarylane_vpc.perth-vpc.id
   public_ipv4_count = 1
