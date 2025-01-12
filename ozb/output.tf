2b5c0d2c271eb6da3dbd450ffffa01b03512fd96
Anurag Singh <contact@anurag.sh>
2025-01-12 14:39:45 +0800

    add things

--- ozb/output.tf
+++
@@ -1,4 +0,0 @@
-output "mongodb-dev-connection-string" {
-  value     = "mongodb+srv://${mongodbatlas_database_user.ozb-user-dev.username}:${random_password.ozb-user-password-dev.result}@${trimprefix(mongodbatlas_cluster.this.srv_address, "mongodb+srv://")}/${var.dev-database-name}"
-  sensitive = true
-}
--- ozb/secrets.tf
+++ ozb/secrets.tf
@@ -1,27 +1,3 @@
-resource "aws_secretsmanager_secret" "bot-secret-discord-token" {
-  name = "Ozb-DiscordAuthToken"
-}
-
-resource "aws_secretsmanager_secret_version" "bot-secret-discord-token" {
-  secret_id     = aws_secretsmanager_secret.bot-secret-discord-token.id
-  secret_string = "REPLACED_IN_PORTAL"
-
-  lifecycle {
-    ignore_changes = [
-      secret_string
-    ]
-  }
-}
-
-resource "aws_secretsmanager_secret" "mongodb-connection-string" {
-  name = "Ozb-MongoDbConnectionString"
-}
-
-resource "aws_secretsmanager_secret_version" "mongodb-connection-string" {
-  secret_id     = aws_secretsmanager_secret.mongodb-connection-string.id
-  secret_string = "mongodb+srv://${mongodbatlas_database_user.ozb-user.username}:${random_password.ozb-user-password.result}@${trimprefix(mongodbatlas_cluster.this.srv_address, "mongodb+srv://")}/${var.database-name}"
-}
-
 resource "random_password" "ozb-user-password" {
   length  = 50
   special = false
@@ -32,7 +8,3 @@ resource "random_password" "ozb-user-password-dev" {
   special = false
 }
 
-module "axiom-token" {
-  source      = "../module/keyvault-value-output"
-  secret_name = "ozb-axiom-token"
-}
