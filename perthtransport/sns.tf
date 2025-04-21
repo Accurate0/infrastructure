2e84d1275b93800b25035b86f260e242e6422f12
Anurag Singh <contact@anurag.sh>
2025-04-21 21:41:14 +0800

    remove unused

--- perthtransport/sns.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_sns_topic" "alert-notification" {
-  name = "perthtransport-alert-topic"
-}
--- summarise/secrets.tf
+++
@@ -1,9 +0,0 @@
-module "coolify-api-readonly-secret" {
-  source      = "../module/keyvault-value-output"
-  secret_name = "coolify-api-deploy-secret"
-}
-
-module "coolify-api-webhook" {
-  source      = "../module/keyvault-value-output"
-  secret_name = "coolify-api-summarise-webhook-url"
-}
